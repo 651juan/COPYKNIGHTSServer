5340e14cfb9cda6c12c44250b0e6da1124adca9b
Juan Buhagiar <juan.buhagiar@iaeste.org.mt>
2016-09-29 12:26:35 +0200

    Initial Server Commit

---
+++ src/main/java/com/eu/article/Application.java
@@ -0,0 +1,17 @@
+package com.eu.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Juan on 29/09/2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
